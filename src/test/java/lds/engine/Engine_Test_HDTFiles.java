0152c34f198115525137b57f533340116f908038
FouadKom <fouadkomeiha@gmail.com>
2020-01-17 11:32:13 +0100

    Creating LdIndexManager class to manage all indexes

---
+++ src/test/java/lds/engine/Engine_Test_HDTFiles.java
@@ -0,0 +1,111 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lds.engine;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import lds.measures.Measure;
+import lds.resource.R;
+import org.junit.Test;
+import sc.research.ldq.LdDataset;
+import slib.utils.i.Conf;
+import test.utility.Util;
+
+/**
+ *
+ * @author Fouad Komeiha
+ */
+public class Engine_Test_HDTFiles {
+    
+public static final String dataSetDir = System.getProperty("user.dir") + "/src/test/resources/dbpedia2016-04en.hdt";
+     
+    @Test
+    public void Engine_Test_HDTvsDBpedia() throws InterruptedException, ExecutionException, Exception{
+            
+            double startTime , endTime , duration;
+            
+            LdDataset dbpedia = Util.getDBpediaDataset();
+            
+            LdDataset hdt = Util.getDBpediaHDTDataset(dataSetDir);
+            
+            Util.SplitedList sp = Util.splitList(Util.getHDTResources(2, dataSetDir));
+//            Util.SplitedList sp = Util.splitList(Util.getDbpediaResources(2));
+
+            //get two list of Dbpedia resources
+            List<R> listOfResources1 = sp.getFirstList();
+            List<R> listOfResources2 = sp.getSecondList();
+            
+            Conf config = new Conf();
+            LdSimilarityEngine engine = new LdSimilarityEngine();
+            
+            ///////////////////////LDSD_cw////////////////////////////////////////////////////////////////////////
+            config.addParam("useIndexes", false);
+            config.addParam("LdDatasetMain" , hdt);
+            
+            double sum = 0;            
+            
+            engine.load(Measure.LDSD_cw, config);
+            
+            startTime = System.nanoTime();
+            sum = 0;
+            for(int i = 0 ; i < listOfResources1.size() ; i++){
+                sum = sum + engine.similarity(listOfResources1.get(i) , listOfResources2.get(i));
+            }
+            
+            //end timing
+            endTime = System.nanoTime();
+            duration = (endTime - startTime) / 1000000000 ;
+            System.out.println("Comparing " + listOfResources1.size() + " pairs using LDSD_dw from hdt finished in " + duration + " second(s) ");
+            System.out.println("Sum comparing = " + sum);
+            System.out.println();
+            
+            engine.close();
+            
+            ///////////////////////Resim////////////////////////////////////////////////////////////////////////   
+/*          double sum = 0;  
+            config.addParam("useIndexes", false);
+            config.addParam("LdDatasetMain" , hdt);
+            engine.load(Measure.Resim, config);
+            
+            startTime = System.nanoTime();
+            
+            for(int i = 0 ; i < listOfResources1.size() ; i++){
+                engine.similarity(listOfResources1.get(i) , listOfResources2.get(i));
+            }
+            
+            //end timing
+            endTime = System.nanoTime();
+            duration = (endTime - startTime) / 1000000000 ;
+            System.out.println("Comparing " + listOfResources1.size() + " pairs using Resim from hdt finished in " + duration + " second(s) ");
+            System.out.println();
+            
+            engine.close();
+            ///////////////////////PICSS///////////////////////////////////////////////////////////////////////
+            config.addParam("resourcesCount" , 2350906);
+            config.addParam("useIndexes", false);           
+            config.addParam("LdDatasetMain" , hdt);
+            engine.load(Measure.PICSS, config);
+            
+            startTime = System.nanoTime();
+            
+            for(int i = 0 ; i < listOfResources1.size() ; i++){
+                engine.similarity(listOfResources1.get(i) , listOfResources2.get(i));
+            }
+            
+            //end timing
+            endTime = System.nanoTime();
+            duration = (endTime - startTime) / 1000000000 ;
+            System.out.println("Comparing " + listOfResources1.size() + " pairs using PICSS from hdt finished in " + duration + " second(s) ");
+            System.out.println();
+            
+            
+            engine.close();
+    */       
+
+	}
+    
+}
+
--- src/test/java/lds/measures/epics/measureTest_DBpedia.java
+++ src/test/java/lds/measures/epics/measureTest_DBpedia.java
@@ -28,11 +28,11 @@ public class measureTest_DBpedia {
       
         LdDataset dataset = Util.getDBpediaDataset();
         
-        R r1 = LdResourceFactory.getInstance().baseUri("http://dbpedia.org/resource/").name("Airplane").create();
+        R r1 = LdResourceFactory.getInstance().baseUri("http://dbpedia.org/resource/").name("Automobile").create();
         R r2 = LdResourceFactory.getInstance().baseUri("http://dbpedia.org/resource/").name("Car").create();
                 
         Conf config = new Conf();
-        config.addParam("useIndexes", true);
+        config.addParam("useIndexes", false);
         config.addParam("LdDatasetMain" , dataset);
         config.addParam("resourcesCount" , 2350906);
         config.addParam("extendingMeasure" , "LDSD_d");
@@ -42,13 +42,13 @@ public class measureTest_DBpedia {
         
         
         picss.loadIndexes();
-        epics.loadIndexes();
+//        epics.loadIndexes();
         
-        System.out.println("PICSS(r1 , r2) = " + picss.compare(r1, r2));
+//        System.out.println("PICSS(r1 , r2) = " + picss.compare(r1, r2));
         System.out.println("EPICS(r1 , r2) = " + epics.compare(r1, r2));
         
         picss.closeIndexes();
-        epics.closeIndexes();
+//        epics.closeIndexes();
         
     }
     
